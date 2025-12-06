b533b0bb8fcef9b4b201ac23811d4d1865ed885c
IgnacioAlvarezmda <ignacioalvarezmardeajo@gmail.com>
2025-12-06 16:23:37 -0300

    commit 5

--- script.js
+++ script.js
@@ -1,8 +1,33 @@
-flecha= document.getElementById("flecha-abajo")
+const flecha= document.getElementById("flecha-abajo")
 
 window.addEventListener("scroll",()=>{
     if(window.scrollY>100){
         flecha.classList.add("desaparecer")
 }})
 
-contador= document.getElementById("cuenta_atras")
+const contador= document.getElementById("cuenta_atras")
+
+
+
+
+
+const botonllegar= document.getElementById("boton-llegar")
+const destino= document.getElementById("seccion-mapa")
+
+botonllegar.addEventListener("click",()=>{
+    if (destino){
+        destino.scrollIntoView({behavior:"smooth"})
+    }
+})
+
+const botonentradas= document.getElementById("boton")
+const nombre= document.getElementById("nombre").value;
+const email= document.getElementById("email").value;
+const cantidad= document.getElementById("cantidad").value;
+
+botonentradas.addEventListener("click",(e)=>{
+    if (nombre==""||email==""||cantidad==""){
+        alert ("Por favor completa todos los campos del formulario correctamente.")
+        return;
+        alert("Gracias por solicitar informacion, revisá tu correo electrónico para ver los detalles de las entradas. :) ")
+    }})
