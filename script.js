2351ccce149e3f371f857c89c52c344e4316c595
IgnacioAlvarezmda <ignacioalvarezmardeajo@gmail.com>
2025-12-10 20:45:24 -0300

    commit 9

--- script.js
+++ script.js
@@ -1,14 +1,10 @@
+
 const flecha= document.getElementById("flecha-abajo")
 
 window.addEventListener("scroll",()=>{
     if(window.scrollY>100){
         flecha.classList.add("desaparecer")
 }})
-
-const contador= document.getElementById("cuenta_atras")
-
-
-
 
 
 const botonllegar= document.getElementById("boton-llegar")
@@ -25,14 +21,8 @@ const nombre= document.getElementById("nombre").value;
 const email= document.getElementById("email").value;
 const cantidad= document.getElementById("cantidad").value;
 
-botonentradas.addEventListener("click",(e)=>{
-    if (nombre==""||email==""||cantidad==""){
-        alert ("Por favor completa todos los campos del formulario correctamente.")
-        return prompt("Gracias por solicitar informacion, revisa tu correo electrónico para ver los detalles de las entradas. :)");
-    }})
 
     const tarjetascombates= document.querySelectorAll(".combates")
-    
 
     tarjetascombates.forEach((tarjeta)=>{
 
